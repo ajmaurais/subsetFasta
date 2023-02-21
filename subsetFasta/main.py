a8c40c366a3fd7ba6451a143bf2b35e3e3226e0c
mauraisa <mauraisa@uw.edu>
2023-02-21 15:09:21 -0800

    Add --multiLine option.

--- subsetFasta/main.py
+++ subsetFasta/main.py
@@ -3,7 +3,7 @@ import sys
 import os
 import argparse
 
-from submodules.fasta import FastaFile
+from .submodules.fasta import FastaFile, format_sequence
 
 def readIdsFromFile(fname):
     with open(fname, 'r') as inF:
@@ -11,15 +11,18 @@ def readIdsFromFile(fname):
     return set(lines)
 
 
-def writeEntries(fname, entries):
+def writeEntries(fname, entries, multi_line=False):
     with open(fname, 'w') as outF:
-        for entry in entries:
-            outF.write('>sp|{}|{}\n{}\n'.format(*entry))
+        for accession, description, seq in entries:
+            outF.write('>sp|{}|{}\n{}\n'.format(accession, description,
+                                                format_sequence(seq, 60) if multi_line else seq))
 
 
 def main():
     parser = argparse.ArgumentParser('Subset fasta based off a list of uniprot IDs.')
     parser.add_argument('-f', '--file', default=None, help='Obtain ids from a file')
+    parser.add_argument('--multiLine', default=False, action='store_true',
+                        help='Add line break every 60 characters in protein sequences.')
     parser.add_argument('fasta', help='The fasta file to filter')
     parser.add_argument('ids', nargs='*', help='The IDs to select.')
     args = parser.parse_args()
@@ -37,7 +40,7 @@ def main():
     fasta.read(args.fasta)
 
     entries = [(protein, *fasta.get_entry(protein)) for protein in select_ids]
-    writeEntries(ofname, entries)
+    writeEntries(ofname, entries, multi_line=args.multiLine)
 
 if __name__ == '__main__':
     main()
